717ca90321ef8a54c69a2c3b70871633cad0635f
Stefan Mertl <info@stefanmertl.com>
2011-12-17 14:12:44 +0100

    Removed some old documentation strings.

--- src/psysmon/core/editDialog.py
+++ src/psysmon/core/editDialog.py
@@ -1,36 +1,4 @@
-## @file psysmon.core.editDialog.py
-# 
 
-## @package psysmon.core.editDialog
-#
-#
-#
-# @author: Stefan Mertl
-# Created on Jun 27, 2011
-
-## @page psysmon.core.editDialog_page  Page for module psysmon.core.editDialog
-#
-# @section sec_intro Introduction
-#
-#
-# @section sec_license License
-#     This file is part of Seismon.
-#
-#     If you use Seismon in any program or publication, please inform and
-#     acknowledge its author Stefan Mertl (info@stefanmertl.com). 
-# 
-#     Seismon is free software: you can redistribute it and/or modify
-#     it under the terms of the GNU General Public License as published by
-#     the Free Software Foundation, either version 3 of the License, or
-#     (at your option) any later version.
-# 
-#     This program is distributed in the hope that it will be useful,
-#     but WITHOUT ANY WARRANTY; without even the implied warranty of
-#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#     GNU General Public License for more details.
-# 
-#     You should have received a copy of the GNU General Public License
-#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import wx
 from wx.lib.stattext import GenStaticText as StaticText
--- src/psysmon/packages/selectWaveform/selectWaveform.py
+++ src/psysmon/packages/selectWaveform/selectWaveform.py
@@ -1,36 +1,4 @@
-## @file psysmon.packages.obspyImportWaveform.importWaveform.py
-# 
 
-## @package psysmon.packages.obspyImportWaveform.importWaveform
-#
-#
-#
-# @author: Stefan Mertl
-# Created on Apr 25, 2011
-
-## @page psysmon.packages.obspyImportWaveform.importWaveform_page  Page for module psysmon.packages.obspyImportWaveform.importWaveform
-#
-# @section sec_intro Introduction
-#
-#
-# @section sec_license License
-#     This file is part of Seismon.
-#
-#     If you use Seismon in any program or publication, please inform and
-#     acknowledge its author Stefan Mertl (info@stefanmertl.com).
-#
-#     Seismon is free software: you can redistribute it and/or modify
-#     it under the terms of the GNU General Public License as published by
-#     the Free Software Foundation, either version 3 of the License, or
-#     (at your option) any later version.
-#
-#     This program is distributed in the hope that it will be useful,
-#     but WITHOUT ANY WARRANTY; without even the implied warranty of
-#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#     GNU General Public License for more details.
-#
-#     You should have received a copy of the GNU General Public License
-#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from psysmon.core.packageNodes import CollectionNode
 from psysmon.core.util import PsysmonError 
