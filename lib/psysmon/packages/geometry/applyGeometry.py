7382b19771cc2ea90cd48045b47dc8191ce96b5b
Stefan Mertl <info@stefanmertl.com>
2012-02-24 10:39:57 +0100

    Changed the database queries to use the sqlAlchemy DAL.

--- lib/psysmon/packages/geometry/applyGeometry.py
+++ lib/psysmon/packages/geometry/applyGeometry.py
@@ -19,6 +19,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from psysmon.core.packageNodes import CollectionNode
+import logging
 
 ## The ApplyGeometry class.
 # 
@@ -30,15 +31,15 @@ from psysmon.core.packageNodes import CollectionNode
 #
 # The apply geometry collection node is an uneditable node.
 class ApplyGeometry(CollectionNode):
-    
-        
+
+
     ## The edit method.
     #
     # The EditGeometry node is an uneditable node - ignore the edit method.
     def edit(self):
         pass
-        
-        
+
+
     ## The execute method.
     # 
     # Transfer the geometry of the current inventory to the traceheader 
@@ -52,30 +53,38 @@ class ApplyGeometry(CollectionNode):
     # @param prevNodeOutput The output of the previous collection node. 
     # Not used in this method.
     def execute(self, prevNodeOutput={}):
-        stationTable = self.project.dbTableNames['geom_station']
-        recorderTable = self.project.dbTableNames['geom_recorder']
-        sensorTable = self.project.dbTableNames['geom_sensor']
-        sensorTimeTable = self.project.dbTableNames['geom_sensor_time']
-        traceheaderTable = self.project.dbTableNames['traceheader']
-        
-        
+        loggerName = __name__ + "." + self.__class__.__name__
+        self.logger = logging.getLogger(loggerName)
+
+        stationTable = self.project.dbTables['geom_station']
+        recorderTable = self.project.dbTables['geom_recorder']
+        sensorTable = self.project.dbTables['geom_sensor']
+        sensorTimeTable = self.project.dbTables['geom_sensor_time']
+        traceheaderTable = self.project.dbTables['traceheader']
+
+        dbSession = self.project.getDbSession()
+
         # Reset all files in the traceheader table. Set the station_id, recorder_id  
         # and sensor_id to -1.
-        query =  ("UPDATE %s "
-                  "SET recorder_id=-1, station_id=-1, sensor_id=-1 ") % traceheaderTable
-                  
-        res = self.project.executeQuery(query)
-        
-        if not res['isError']:
-            self.log("status", "Successfully reset the traceheader geometry.")
-        else:
-            self.log("error", res['msg'])  
-            return 
-        
-        
+        #query =  ("UPDATE %s "
+        #          "SET recorder_id=-1, station_id=-1, sensor_id=-1 ") % traceheaderTable
+
+        dbSession.query(traceheaderTable).update(values=dict(recorder_id=None, station_id=None, sensor_id=None))
+        dbSession.commit()
+
+        #if not res['isError']:
+        #    self.log("status", "Successfully reset the traceheader geometry.")
+        #else:
+        #    self.log("error", res['msg'])  
+        #    return 
+
+        #dbSession.query(traceheaderTable).filter(recorderTable.serial == traceheaderTable.recorder_serial).update(values=dict(recorder_id=recorderTable.id))
+        #dbSession.commit()
+        #return
+
         # Assign the station-, recorder- and sensor ids to the fileheaders in 
         # the traceheader table.
-        query =  ("UPDATE %s as th, %s as rec, %s as sens, %s as sensTime, %s as stat "
+        query =  ("PDATE %s as th, %s as rec, %s as sens, %s as sensTime, %s as stat "
                   "SET th.recorder_id = rec.id, th.station_id = stat.id, th.sensor_id = sens.id "
                   "WHERE "
                   "rec.serial LIKE th.recorder_serial "
@@ -83,15 +92,22 @@ class ApplyGeometry(CollectionNode):
                   "AND th.channel = sens.rec_channel_name "
                   "AND sensTime.sensor_id = sens.id "
                   "AND sensTime.stat_id = stat.id "
-                  "AND (th.begin_time >= sensTime.start_time AND (th.begin_time <= sensTime.end_time OR sensTime.end_time IS NULL))") % (traceheaderTable, recorderTable, sensorTable, sensorTimeTable, stationTable) 
-                  
-        res = self.project.executeQuery(query)
-        
-        if not res['isError']:
-            self.log("status", "Successfully updated the traceheader geometry.")
-        else:
-            self.log("error", res['msg'])  
-            return 
-        
-        
-        
+                  "AND (th.begin_time >= sensTime.start_time AND (th.begin_time <= sensTime.end_time OR sensTime.end_time IS NULL))") % (traceheaderTable.__table__.name, 
+                                                                                                                                         recorderTable.__table__.name, 
+                                                                                                                                         sensorTable.__table__.name, 
+                                                                                                                                         sensorTimeTable.__table__.name, 
+                                                                                                                                         stationTable.__table__.name) 
+        try:
+            dbSession.execute(query)
+        except:
+            self.log("error", "Database error")
+            self.logger.error("Database error")
+
+        #if not res['isError']:
+        #    self.log("status", "Successfully updated the traceheader geometry.")
+        #else:
+        #    self.log("error", res['msg'])  
+        #    return 
+
+
+
