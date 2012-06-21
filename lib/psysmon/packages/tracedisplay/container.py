bbfc1d859d4f5f9a2c6352dab210c28fd3e82d85
Stefan Mertl <info@stefanmertl.com>
2012-06-21 12:49:53 +0200

    Added a dictionary to map the view types to the python classes.

--- lib/psysmon/packages/tracedisplay/container.py
+++ lib/psysmon/packages/tracedisplay/container.py
@@ -14,6 +14,7 @@ import numpy as np
 
 
 
+
 class TdViewAnnotationPanel(wx.Panel):
     '''
     The view annotation area.
@@ -880,3 +881,4 @@ class TdViewPort(scrolled.ScrolledPanel):
 
 
 
+viewTypeMap = {'seismogram' : SeismogramView}
