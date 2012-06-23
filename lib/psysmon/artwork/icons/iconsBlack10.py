a95ef53ba7bb7849b75f97d2d435917c44f0a9de
Stefan Mertl <info@stefanmertl.com>
2012-06-23 11:23:52 +0200

    Recomputed the embedded image file.

--- lib/psysmon/artwork/icons/iconsBlack10.py
+++ lib/psysmon/artwork/icons/iconsBlack10.py
@@ -1,37 +1,37 @@
 from wx.lib.embeddedimage import PyEmbeddedImage
 
 #----------------------------------------------------------------------
-plus_icon10 = PyEmbeddedImage(
-    "iVBORw0KGgoAAAANSUhEUgAAAAkAAAAJCAYAAADgkQYQAAAABHNCSVQICAgIfAhkiAAAAAlw"
-    "SFlzAAAN1wAADdcBQiibeAAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoA"
-    "AAB5SURBVBiVfc+xCYJREAPg78Q9FGws7RQsFAtBOwt3cBy3sHGAv7ByDisH8dmc8P6HGggh"
-    "uRA4pRQfYo8HpnU+0McWE6zrsC19xTAidlilX6YeImKMgis80/ziLTDDIheO2OCCe5Y6zXfn"
-    "PJz+ffdKLb20WZqjw6jO3y9eTGrWZ96kAAAAAElFTkSuQmCC")
-getplus_icon10Data = plus_icon10.GetData
-getplus_icon10Image = plus_icon10.GetImage
-getplus_icon10Bitmap = plus_icon10.GetBitmap
-getplus_icon10Icon = plus_icon10.GetIcon
-
-#----------------------------------------------------------------------
-minus_icon10 = PyEmbeddedImage(
+minus_icon_10 = PyEmbeddedImage(
     "iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAABHNCSVQICAgIfAhkiAAAAAlw"
     "SFlzAAAN1wAADdcBQiibeAAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoA"
     "AAA3SURBVBiV1c+hDQAgFMTQB4ap2H8dHLiPQkIwCC6pa5pcigg3y1fWExEqGmJDR13FcggN"
     "SB+8nq+LD2Utlh4FAAAAAElFTkSuQmCC")
-getminus_icon10Data = minus_icon10.GetData
-getminus_icon10Image = minus_icon10.GetImage
-getminus_icon10Bitmap = minus_icon10.GetBitmap
-getminus_icon10Icon = minus_icon10.GetIcon
+getminus_icon_10Data = minus_icon_10.GetData
+getminus_icon_10Image = minus_icon_10.GetImage
+getminus_icon_10Bitmap = minus_icon_10.GetBitmap
+getminus_icon_10Icon = minus_icon_10.GetIcon
 
 #----------------------------------------------------------------------
-delete_icon10 = PyEmbeddedImage(
+delete_icon_10 = PyEmbeddedImage(
     "iVBORw0KGgoAAAANSUhEUgAAAAcAAAAHCAYAAADEUlfTAAAABHNCSVQICAgIfAhkiAAAAAlw"
     "SFlzAAAN1wAADdcBQiibeAAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoA"
     "AABxSURBVAiZLckxDgEBFADR9xMX2G03odE7g5PoHcYJVGqFK0iIYkW20HADNTZRfc2vJjMT"
     "2KDFOjPHiGixQw8XJI6YYSjfQ4dnhW/xikZmwgK/Gm9Mq+vwqDEWezRwqnDGHPfywwQ3vLDK"
     "zE9ELLHF8AeUGytHr/GPWAAAAABJRU5ErkJggg==")
-getdelete_icon10Data = delete_icon10.GetData
-getdelete_icon10Image = delete_icon10.GetImage
-getdelete_icon10Bitmap = delete_icon10.GetBitmap
-getdelete_icon10Icon = delete_icon10.GetIcon
+getdelete_icon_10Data = delete_icon_10.GetData
+getdelete_icon_10Image = delete_icon_10.GetImage
+getdelete_icon_10Bitmap = delete_icon_10.GetBitmap
+getdelete_icon_10Icon = delete_icon_10.GetIcon
+
+#----------------------------------------------------------------------
+plus_icon_10 = PyEmbeddedImage(
+    "iVBORw0KGgoAAAANSUhEUgAAAAkAAAAJCAYAAADgkQYQAAAABHNCSVQICAgIfAhkiAAAAAlw"
+    "SFlzAAAN1wAADdcBQiibeAAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoA"
+    "AAB5SURBVBiVfc+xCYJREAPg78Q9FGws7RQsFAtBOwt3cBy3sHGAv7ByDisH8dmc8P6HGggh"
+    "uRA4pRQfYo8HpnU+0McWE6zrsC19xTAidlilX6YeImKMgis80/ziLTDDIheO2OCCe5Y6zXfn"
+    "PJz+ffdKLb20WZqjw6jO3y9eTGrWZ96kAAAAAElFTkSuQmCC")
+getplus_icon_10Data = plus_icon_10.GetData
+getplus_icon_10Image = plus_icon_10.GetImage
+getplus_icon_10Bitmap = plus_icon_10.GetBitmap
+getplus_icon_10Icon = plus_icon_10.GetIcon
 
