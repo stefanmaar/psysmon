17a9896664123a8847ff132f12d27aa3842e5673
Stefan Mertl <info@stefanmertl.com>
2012-07-27 16:04:52 +0200

    Reset the version to 0.0.1. The minor version 0 indicates the development version.

--- lib/psysmon/__init__.py
+++ lib/psysmon/__init__.py
@@ -18,7 +18,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "0.1.0"
+__version__ = "0.0.1"
 __author__ = "Stefan Mertl"
 __authorEmail__ = "info@stefanmertl.com"
 __description__ = "A seismological data processing and prototyping software."
