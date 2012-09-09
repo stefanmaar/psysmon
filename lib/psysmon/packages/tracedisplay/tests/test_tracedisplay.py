c114a78e3f2917dc4dc34bf3e8bcd374bc9ebefd
Stefan Mertl <info@stefanmertl.com>
2012-09-09 10:24:13 +0200

    Moving the project setup to the test util module.

--- lib/psysmon/packages/tracedisplay/tests/test_tracedisplay.py
+++ lib/psysmon/packages/tracedisplay/tests/test_tracedisplay.py
@@ -19,44 +19,7 @@ class TracedisplayTestCase(unittest.TestCase):
     Test suite for psysmon.packages.geometry.editGeometry.EditGeometryDlg
     """
     def setUp(self):
-        # Configure the logger.
-        logger = logging.getLogger('psysmon')
-        logger.setLevel(psysmon.logConfig['level'])
-        logger.addHandler(psysmon.getLoggerHandler())
-
-        # Get the pSysmon base directory.
-        psyBaseDir = '/home/stefan/01_gtd/04_aktuelleProjekte/pSysmon/01_src/psysmon/lib/psysmon/'
-        psyBaseDir = os.path.dirname(psyBaseDir)
-
-        # Initialize the pSysmon base object.
-        psyBase = Base(psyBaseDir)
-        #psyBase.scan4Package()
-
-        # Load the pSysmon test project.
-        path = "/home/stefan/01_gtd/04_aktuelleProjekte/pSysmon/03_pSysmonProjects/test/test.ppr"
-        psyBase.loadPsysmonProject(path)
-
-        # Quest for the user and the database password.
-        psyBase.project.setActiveUser('stefan','')
-
-        # Load the database structure of the project packages.
-        psyBase.project.loadDatabaseStructure(psyBase.packageMgr.packages)
-
-        # Create the project waveclient.
-        waveclient = PsysmonDbWaveClient('main client', psyBase.project)
-        psyBase.project.addWaveClient(waveclient)
-        waveclient = EarthwormWaveClient('earthworm localhost')
-        psyBase.project.addWaveClient(waveclient)
-        self.app =psygui.PSysmonApp()
-
-        nodeTemplate = psyBase.packageMgr.getCollectionNodeTemplate('tracedisplay')
-        self.node = copy.deepcopy(nodeTemplate)
-        self.node.project = psyBase.project
-
-        # Create the node logger. This is usually done in the collection.
-        loggerName = __name__ + "." + self.node.__class__.__name__
-        self.node.logger = logging.getLogger(loggerName)
-
+        pass
 
         #self.dlg = EditGeometryDlg(node, psyBase.project)
         #self.dlg.Show()
