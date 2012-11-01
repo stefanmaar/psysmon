f0656722ff2591c187e6daa1c443e7f7b0bf1304
Stefan Mertl <info@stefanmertl.com>
2012-11-01 16:10:06 +0100

    Updated the tracedisplay test to run the GUI without having to run the main pSysmon script.

--- lib/psysmon/packages/tracedisplay/tests/test_tracedisplay.py
+++ lib/psysmon/packages/tracedisplay/tests/test_tracedisplay.py
@@ -7,23 +7,53 @@ Created on May 17, 2011
 import unittest
 import psysmon
 import logging
-from psysmon.core.base import Base
-from psysmon.core.waveclient import PsysmonDbWaveClient,EarthwormWaveClient
-import psysmon.core.gui as psygui
 import os
-import copy
+import glob
+from psysmon.core.test_util import create_psybase
+from psysmon.core.test_util import create_full_project
+from psysmon.core.test_util import drop_project_database_tables
+from psysmon.core.test_util import remove_project_filestructure
+import psysmon.core.gui as psygui
+from psysmon.packages.geometry.inventory import Inventory
+from psysmon.packages.geometry.inventory import InventoryDatabaseController
 
 
 class TracedisplayTestCase(unittest.TestCase):
     """
     Test suite for psysmon.packages.geometry.editGeometry.EditGeometryDlg
     """
+    @classmethod
+    def setUpClass(cls):
+        cls.psybase = create_psybase()
+        create_full_project(cls.psybase)
+        cls.project = cls.psybase.project
+        print "In setUpClass...\n"
+
+
+    @classmethod
+    def tearDownClass(cls):
+        drop_project_database_tables(cls.project)
+        remove_project_filestructure(cls.project)
+        os.removedirs(cls.project.base_dir)
+        print "....in tearDownClass.\n"
+
+
     def setUp(self):
-        pass
+        # Configure the logger.
+        logger = logging.getLogger('psysmon')
+        logger.setLevel('DEBUG')
+        logger.addHandler(psysmon.getLoggerHandler())
 
-        #self.dlg = EditGeometryDlg(node, psyBase.project)
-        #self.dlg.Show()
 
+        self.app =psygui.PSysmonApp()
+
+        nodeTemplate = self.psybase.packageMgr.getCollectionNodeTemplate('tracedisplay')
+        self.node = nodeTemplate()
+        self.node.project = self.project
+
+        # Create a logger for the node.
+        loggerName = __name__+ "." + self.node.__class__.__name__
+        self.node.logger = logging.getLogger(loggerName)
 
     def tearDown(self):
         print "\n\nEs war sehr schoen - auf Wiederseh'n.\n"
@@ -33,9 +63,6 @@ class TracedisplayTestCase(unittest.TestCase):
         self.app.MainLoop()
 
 
-#def suite():
-#    suite = unittest.makeSuite(EditGeometryDlgTestCase, 'test')
-#    return suite
 
 def suite():
     # return unittest.TestSuite(map(EditGeometryDlgTestCase, tests))
