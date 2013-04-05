181ccbccab94e8f09b4e04e615f3c225a909957c
Stefan Mertl <stefan@hausmeister.(none)>
2013-04-05 18:25:06 +0200

    Added a test for the fileImport dialog.

---
+++ lib/psysmon/packages/obspyImportWaveform/tests/test_importWaveformDlg.py
@@ -0,0 +1,74 @@
+'''
+Created on May 17, 2011
+
+@author: Stefan Mertl
+'''
+
+import unittest
+import psysmon
+import logging
+import os
+from psysmon.core.test_util import create_psybase
+from psysmon.core.test_util import create_empty_project
+from psysmon.core.test_util import drop_project_database_tables
+from psysmon.core.test_util import remove_project_filestructure
+import psysmon.core.gui as psygui
+
+
+class ImportWaveformEditDlgTestCase(unittest.TestCase):
+    """
+    Test suite for psysmon.packages.obspyImportWaveform.ImportWaveformEditDlg
+    """
+    @classmethod
+    def setUpClass(cls):
+        # Configure the logger.
+        logger = logging.getLogger('psysmon')
+        logger.setLevel('DEBUG')
+        logger.addHandler(psysmon.getLoggerHandler())
+
+        cls.psybase = create_psybase()
+        cls.project = create_empty_project(cls.psybase)
+        print "In setUpClass...\n"
+
+
+    @classmethod
+    def tearDownClass(cls):
+        print "dropping database tables...\n"
+        drop_project_database_tables(cls.project)
+        print "removing temporary file structure....\n"
+        remove_project_filestructure(cls.project)
+        print "removing temporary base directory....\n"
+        os.removedirs(cls.project.base_dir)
+        print "....finished cleaning up.\n"
+
+
+    def setUp(self):
+        self.app =psygui.PSysmonApp()
+
+        nodeTemplate = self.psybase.packageMgr.getCollectionNodeTemplate('import waveform')
+        self.node = nodeTemplate()
+        self.node.project = self.project
+
+        # Create a logger for the node.
+        loggerName = __name__+ "." + self.node.__class__.__name__
+        self.node.logger = logging.getLogger(loggerName)
+
+
+    def tearDown(self):
+        print "\n\nEs war sehr schoen - auf Wiederseh'n.\n"
+
+    def testDlg(self):
+        self.node.edit()
+        self.app.MainLoop()
+
+#def suite():
+#    suite = unittest.makeSuite(EditGeometryDlgTestCase, 'test')
+#    return suite
+
+def suite():
+    # return unittest.TestSuite(map(EditGeometryDlgTestCase, tests))
+    return unittest.makeSuite(ImportWaveformEditDlgTestCase, 'test')
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='suite')
+
