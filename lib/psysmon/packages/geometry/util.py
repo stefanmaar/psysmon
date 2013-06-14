daade947fcfc3e0e7900ec6dee519c9d2233e8eb
Stefan Mertl <stefan@hausmeister.(none)>
2013-06-14 10:51:58 +0200

    Added a method to build an epsg dictionary from the basemap epsg proj file. Added a method to extract the epsg code from a proj srs string.

--- lib/psysmon/packages/geometry/util.py
+++ lib/psysmon/packages/geometry/util.py
@@ -1,3 +1,4 @@
+import ipdb
 # LICENSE
 #
 # This file is part of pSysmon.
@@ -54,5 +55,89 @@ def zone2UtmCentralMeridian(zone):
 
     return zone * 6 - 180 - 3
 
+def epsg_from_srs(srs):
+    ''' Extract the epsg code from a proj srs string.
+
+    '''
+    l = srs.split()
+    for s in l:
+        print s
+        try:
+            k,v = s.split('=')
+        except:
+            continue
+        k = k.strip('+')
+        if k == 'init':
+            return v
+
+
+def get_epsg_dict():
+    ''' Create a dictionary for mapping proj projection arguments to epsg codes.
+
+    This function is a modified version of the one included in mpl_toolkits.basemap.
+    It reads the epsg file in the matplotlib data directory and creates a dictionary 
+    with the epsg codes as the keys and the responding proj projection arguments as 
+    the values.
+    '''
+    # create dictionary that maps epsg codes to Basemap kwargs.
+    import mpl_toolkits.basemap
+    import os
+    epsgf = open(os.path.join(mpl_toolkits.basemap.basemap_datadir,'epsg'))
+    epsg_dict={}
+    for line in epsgf:
+        if line.startswith("#"):
+            continue
+        l = line.split()
+        code = l[0].strip("<>")
+        parms = ' '.join(l[1:-1])
+        _kw_args={}
+        for s in l[1:-1]:
+            try:
+                k,v = s.split('=')
+            except:
+                k = s.strip()
+                v = None
+            k = k.strip("+")
+            if k=='proj':
+                if v == 'longlat': v = 'cyl'
+                k='projection'
+            if k=='k':
+                k='k_0'
+            if k in ['projection','lat_1','lat_2','lon_0','lat_0',\
+                     'a','b','k_0','lat_ts','ellps','datum', 'zone', 'units']:
+                if k not in ['projection','ellps','datum', 'units']:
+                    v = float(v)
+                _kw_args[k]=v
+            else:
+                _kw_args[k] = True
+        if 'projection' in _kw_args:
+            if 'a' in _kw_args:
+                if 'b' in _kw_args:
+                    _kw_args['rsphere']=(_kw_args['a'],_kw_args['b'])
+                    del _kw_args['b']
+                else:
+                    _kw_args['rsphere']=_kw_args['a']
+                del _kw_args['a']
+            if 'datum' in _kw_args:
+                if _kw_args['datum'] == 'NAD83':
+                    _kw_args['ellps'] = 'GRS80'
+                elif _kw_args['datum'] == 'NAD27':
+                    _kw_args['ellps'] = 'clrk66'
+                elif _kw_args['datum'] == 'WGS84':
+                    _kw_args['ellps'] = 'WGS84'
+                del _kw_args['datum']
+            # supported epsg projections.
+            # omerc not supported yet, since we can't handle
+            # alpha,gamma and lonc keywords.
+            if _kw_args['projection'] != 'omerc':
+                epsg_dict[code]=_kw_args
+    epsgf.close()
+    return epsg_dict
+
+
 ellipsoids = {}
 ellipsoids['wgs84'] = (6378137, 6356752.314245179)
+
+
+
+
