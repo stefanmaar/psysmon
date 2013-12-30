5368bd7979ed0557d3062294fcc0b87771415359
Stefan Mertl <info@mertl-research.at>
2013-12-30 21:06:47 +0100

    Updated the test case.

--- lib/psysmon/core/tests/test_base.py
+++ lib/psysmon/core/tests/test_base.py
@@ -5,35 +5,31 @@ Created on May 17, 2011
 '''
 
 import unittest
-from psysmon.core.test_util import create_psybase
-from psysmon.core.test_util import remove_project_filestructure
-from psysmon.core.test_util import drop_project_database_tables
+import psysmon.core.test_util as test_util
 import tempfile
 import os
+import shutil
 
-class ProjectTestCase(unittest.TestCase):
+class BaseTestCase(unittest.TestCase):
     """
-    Test suite for psysmon.packages.geometry.editGeometry.EditGeometryDlg
     """
 
     @classmethod
     def setUpClass(cls):
         print "In setUpClass...\n"
-        cls.psybase = create_psybase()
-        cls.base_dir = tempfile.mkdtemp()
 
 
     @classmethod
     def tearDownClass(cls):
         print "....in tearDownClass.\n"
-        #os.removedir(cls.base_dir)
 
 
     def setUp(self):
-        pass
+        self.psybase = test_util.create_psybase()
+        self.base_dir = tempfile.mkdtemp()
 
     def tearDown(self):
-        pass
+        shutil.rmtree(self.base_dir)
 
 
 
@@ -44,7 +40,7 @@ class ProjectTestCase(unittest.TestCase):
         base_dir = self.base_dir
         db_host = 'localhost'
         user_name = 'unit_test'
-        user_pwd = ''
+        user_pwd = 'test'
         author_name = 'Stefan Test'
         author_uri = 'stest'
         agency_name = 'University of Test'
@@ -53,8 +49,8 @@ class ProjectTestCase(unittest.TestCase):
         self.psybase.createPsysmonProject(name, base_dir, db_host, user_name, user_pwd, 
                                           author_name, author_uri, agency_name, agency_uri)
 
-        drop_project_database_tables(self.psybase.project)
-        remove_project_filestructure(self.psybase.project)
+        test_util.drop_project_database_tables(self.psybase.project)
+        test_util.remove_project_filestructure(self.psybase.project)
         self.psybase.closePsysmonProject()
 
 
@@ -65,7 +61,7 @@ class ProjectTestCase(unittest.TestCase):
         base_dir = self.base_dir
         db_host = 'localhost'
         user_name = 'unit_test'
-        user_pwd = ''
+        user_pwd = 'test'
         author_name = 'Stefan Test'
         author_uri = 'stest'
         agency_name = 'University of Test'
@@ -83,13 +79,13 @@ class ProjectTestCase(unittest.TestCase):
 
         self.psybase.loadPsysmonProject(project_file, user_name, user_pwd)
 
-        drop_project_database_tables(self.psybase.project)
-        remove_project_filestructure(self.psybase.project)
+        test_util.drop_project_database_tables(self.psybase.project)
+        test_util.remove_project_filestructure(self.psybase.project)
         self.psybase.closePsysmonProject()
 
 
 def suite():
-    return unittest.makeSuite(ProjectTestCase, 'test')
+    return unittest.makeSuite(BaseTestCase, 'test')
 
 
 if __name__ == '__main__':
