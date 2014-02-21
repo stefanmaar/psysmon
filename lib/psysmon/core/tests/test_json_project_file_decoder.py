391795397358df13478dd72eb993ea7011f483af
Stefan Mertl <stefan@hausmeister.(none)>
2014-02-21 16:51:33 +0100

    Added tests for a valid project object.

--- lib/psysmon/core/tests/test_json_project_file_decoder.py
+++ lib/psysmon/core/tests/test_json_project_file_decoder.py
@@ -1,4 +1,5 @@
 import unittest
+import psysmon.core.project
 import psysmon.core.test_util as test_util
 import shutil
 import psysmon.core.util as util
@@ -42,16 +43,40 @@ class ProjectFileDecoderTestCase(unittest.TestCase):
         # Set the createTime of the project to a known value.
         self.db_project.createTime = UTCDateTime('2013-01-01T00:00:00')
 
+        # Add an empty collection.
+        self.db_project.createDirectoryStructure()
+        self.db_project.addCollection('Test Collection')
+
+        # Add a collection node to the collection.
+        node_template = self.psybase.packageMgr.getCollectionNodeTemplate('json plain testnode')
+        self.db_project.addNode2Collection(node_template)
+
+        # Add a collection node containing preferences.
+        node_template = self.psybase.packageMgr.getCollectionNodeTemplate('json preferences testnode')
+        self.db_project.addNode2Collection(node_template)
+
         encoder = util.ProjectFileEncoder()
         decoder = util.ProjectFileDecoder()
         json_project = encoder.encode(self.db_project)
-        print 'THE JSON STRING:'
-        print json_project
-        print '\n'
         project_obj = decoder.decode(json_project)
-        print '\nTHE DECODED OBJECT:'
-        print str(project_obj)
 
+        self.assertIsInstance(project_obj, psysmon.core.project.Project)
+        self.assertEqual(project_obj.name, 'Unit Test')
+        self.assertEqual(project_obj.dbHost, 'localhost')
+        self.assertIsInstance(project_obj.activeUser, psysmon.core.project.User)
+        self.assertEqual(len(project_obj.user), 1)
+        self.assertIsInstance(project_obj.user[0], psysmon.core.project.User)
+        self.assertEqual(project_obj.activeUser.name, 'unit_test')
+        self.assertEqual(project_obj.rid, 'smi:at.uot.stest/psysmon/unit_test')
+        self.assertEqual(len(project_obj.activeUser.collection), 1)
+        self.assertIsInstance(project_obj.activeUser.collection['Test Collection'], psysmon.core.base.Collection)
+        collection = project_obj.activeUser.collection['Test Collection']
+        self.assertEqual(len(collection.nodes), 2)
+        self.assertEqual(collection.nodes[0].__class__.__name__, 'JsonPlainTestNode')
+        self.assertEqual(collection.nodes[1].__class__.__name__, 'JsonPreferencesTestNode')
+
+        # TODO: Add test for the pref_manager of the collection nodes.
+        # TODO: Add test for the pref_manager items.
 
 
 def suite():
