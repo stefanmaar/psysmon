cd6825b944a1f044cb71dcbf679c2ed02e9f709d
Stefan Mertl <stefan@hausmeister.(none)>
2014-02-24 16:07:43 +0100

    Changed an import.

--- lib/psysmon/packages/geometry/inventory.py
+++ lib/psysmon/packages/geometry/inventory.py
@@ -33,7 +33,7 @@ inventory.
 
 import psysmon
 from obspy.core.utcdatetime import UTCDateTime
-from psysmon.core.util import PsysmonError
+from psysmon.core.error import PsysmonError
 from mpl_toolkits.basemap import pyproj
 import warnings
 import logging
