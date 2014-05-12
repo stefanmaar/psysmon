aa4830dd58345c66431ec3d84ba5e4a3e3cba608
Stefan Mertl <info@mertl-research.at>
2014-05-12 10:42:00 +0200

    Added a helper script to clean the unit_test database.

---
+++ scripts/clean_unittest_database.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# LICENSE
+#
+# This file is part of pSysmon.
+#
+# If you use pSysmon in any program or publication, please inform and
+# acknowledge its author Stefan Mertl (stefan@mertl-research.at).
+#
+# pSysmon is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+Remove the database tables of the unittest database.
+
+:copyright:
+    Stefan Mertl
+
+:license:
+    GNU General Public License, Version 3 
+    (http://www.gnu.org/licenses/gpl-3.0.html)
+'''
+
+import psysmon.core.test_util as test_util
+
+def run():
+    test_util.clean_unittest_database()
+
+if __name__ == '__main__':
+    run()
+
+
+
+
