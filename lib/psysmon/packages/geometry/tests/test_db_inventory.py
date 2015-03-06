fcf2536cdaa1511fa68649adb847f797a17d61c1
Stefan Mertl <stefan@mertl-research.at>
2015-03-06 13:05:19 +0100

    Added a test for the creation of a DB Inventory from an inventory read from an XML file.

--- lib/psysmon/packages/geometry/tests/test_db_inventory.py
+++ lib/psysmon/packages/geometry/tests/test_db_inventory.py
@@ -55,6 +55,8 @@ from psysmon.packages.geometry.inventory import SensorComponent
 from psysmon.packages.geometry.inventory import SensorComponentParameter
 from psysmon.packages.geometry.inventory import TimeBox
 
+from psysmon.packages.geometry.inventory_parser import InventoryXmlParser
+
 class DbInventoryTestCase(unittest.TestCase):
     """
     Test suite for psysmon.packages.geometry.editGeometry.EditGeometryDlg
@@ -1148,37 +1150,224 @@ class DbInventoryTestCase(unittest.TestCase):
 
 
     def test_xml_to_db_inventory(self):
-        db_inventory = DbInventory('test', self.project)
-
         xml_file = os.path.join(self.data_path, 'simple_inventory.xml')
         xml_parser = InventoryXmlParser()
         inventory = xml_parser.parse(xml_file)
 
-        for cur_recorder in inventory.recorders:
-            db_inventory.add_recorder(cur_recorder)
+        db_inventory = DbInventory.from_inventory_instance(name = 'test',
+                                                           project = self.project,
+                                                           inventory = inventory)
 
-        for cur_network in inventory.networks:
-            db_inventory.add_network(cur_network)
-
-
+        self.assertEqual(len(db_inventory.sensors), len(inventory.sensors))
         self.assertEqual(len(db_inventory.recorders), len(inventory.recorders))
         self.assertEqual(len(db_inventory.networks), len(inventory.networks))
 
-        for k,cur_recorder in enumerate(db_inventory.recorders):
-            self.assertEqual(len(cur_recorder.sensors), len(inventory.recorders[k].sensors))
+        # Test the sensor.
+        self.assertEqual(len(db_inventory.sensors), 1)
+        cur_sensor = db_inventory.sensors[0]
+        self.assertEqual(cur_sensor.serial, '1417')
+        self.assertEqual(cur_sensor.model, 'Seismonitor 1Hz')
+        self.assertEqual(cur_sensor.producer, 'Geospace')
+        self.assertEqual(cur_sensor.description, 'Sensor description.')
 
-        for k, cur_station in enumerate(db_inventory.networks[0].stations):
-            self.assertEqual(len(cur_station.sensors), len(inventory.networks[0].stations[k].sensors))
+        # Test the sensor components.
+        self.assertEqual(len(cur_sensor.components), 3)
+        cur_component = cur_sensor.get_component(name = 'Z')
+        self.assertEqual(len(cur_component), 1)
+        cur_component = cur_component[0]
+        self.assertEqual(cur_component.name, 'Z')
+        self.assertEqual(cur_component.description, 'Sensor component Z description.')
+        self.assertEqual(len(cur_component.parameters), 1)
+        cur_parameter = cur_component.parameters[0]
+        self.assertEqual(cur_parameter.start_time, UTCDateTime('2009-01-01T00:00:00.000000Z'))
+        self.assertEqual(cur_parameter.end_time, UTCDateTime('2009-02-01T00:00:00.000000Z'))
+        self.assertEqual(cur_parameter.sensitivity, 340.55)
+        self.assertEqual(cur_parameter.sensitivity_units, 'V/m/s')
+        self.assertEqual(cur_parameter.tf_normalization_factor, 0.4)
+        self.assertEqual(cur_parameter.tf_normalization_frequency, 1.0)
+        self.assertEqual(len(cur_parameter.tf_poles), 3)
+        self.assertTrue(complex('-4.44+4.44j') in cur_parameter.tf_poles)
+        self.assertTrue(complex('-4.44-4.44j') in cur_parameter.tf_poles)
+        self.assertTrue(complex('-1.083+0j') in cur_parameter.tf_poles)
+        self.assertEqual(len(cur_parameter.tf_zeros), 3)
+        self.assertEqual(cur_parameter.tf_zeros, [complex(0), complex(0), complex(0)])
 
-        # Commit the changes to the database.
-        db_inventory.commit()
-        db_inventory.close()
 
-        # Load the networks from the database.
-        db_inventory_load = DbInventory('test', self.project)
-        db_inventory_load.load_recorders()
-        db_inventory_load.load_networks()
-        db_inventory_load.close()
+        cur_component = cur_sensor.get_component(name = 'N')
+        self.assertEqual(len(cur_component), 1)
+        cur_component = cur_component[0]
+        self.assertEqual(cur_component.name, 'N')
+        self.assertEqual(cur_component.description, 'Sensor component N description.')
+        self.assertEqual(len(cur_component.parameters), 1)
+        cur_parameter = cur_component.parameters[0]
+        self.assertEqual(cur_parameter.start_time, UTCDateTime('2009-01-01T00:00:00.000000Z'))
+        self.assertEqual(cur_parameter.end_time, UTCDateTime('2009-03-01T00:00:00.000000Z'))
+        self.assertEqual(cur_parameter.sensitivity, 340.55)
+        self.assertEqual(cur_parameter.sensitivity_units, 'V/m/s')
+        self.assertEqual(cur_parameter.tf_normalization_factor, 0.4)
+        self.assertEqual(cur_parameter.tf_normalization_frequency, 1.0)
+        self.assertEqual(len(cur_parameter.tf_poles), 3)
+        self.assertTrue(complex('-4.44+4.44j') in cur_parameter.tf_poles)
+        self.assertTrue(complex('-4.44-4.44j') in cur_parameter.tf_poles)
+        self.assertTrue(complex('-1.083+0j') in cur_parameter.tf_poles)
+        self.assertEqual(len(cur_parameter.tf_zeros), 3)
+        self.assertEqual(cur_parameter.tf_zeros, [complex(0), complex(0), complex(0)])
+
+        cur_component = cur_sensor.get_component(name = 'E')
+        self.assertEqual(len(cur_component), 1)
+        cur_component = cur_component[0]
+        self.assertEqual(cur_component.name, 'E')
+        self.assertEqual(cur_component.description, 'Sensor component E description.')
+        self.assertEqual(len(cur_component.parameters), 1)
+        cur_parameter = cur_component.parameters[0]
+        self.assertEqual(cur_parameter.start_time, UTCDateTime('2009-01-01T00:00:00.000000Z'))
+        self.assertIsNone(cur_parameter.end_time)
+        self.assertEqual(cur_parameter.sensitivity, 340.55)
+        self.assertEqual(cur_parameter.sensitivity_units, 'V/m/s')
+        self.assertEqual(cur_parameter.tf_normalization_factor, 0.4)
+        self.assertEqual(cur_parameter.tf_normalization_frequency, 1.0)
+        self.assertEqual(len(cur_parameter.tf_poles), 3)
+        self.assertTrue(complex('-4.44+4.44j') in cur_parameter.tf_poles)
+        self.assertTrue(complex('-4.44-4.44j') in cur_parameter.tf_poles)
+        self.assertTrue(complex('-1.083+0j') in cur_parameter.tf_poles)
+        self.assertEqual(len(cur_parameter.tf_zeros), 3)
+        self.assertEqual(cur_parameter.tf_zeros, [complex(0), complex(0), complex(0)])
+
+
+
+        # Test the recorders.
+        self.assertEqual(len(db_inventory.recorders), 1)
+        cur_recorder = db_inventory.recorders[0]
+        self.assertEqual(cur_recorder.serial, '9D6C')
+        self.assertEqual(cur_recorder.type, 'Reftek 130-01')
+        self.assertEqual(cur_recorder.description, 'Recorder description.')
+        self.assertEqual(len(cur_recorder.streams), 3)
+
+        # Test the first stream.
+        cur_stream = cur_recorder.streams[0]
+        self.assertEqual(cur_stream.name, '101')
+        self.assertEqual(cur_stream.label, 'Stream-101')
+        self.assertEqual(len(cur_stream.parameters), 1)
+        self.assertEqual(len(cur_stream.components), 1)
+
+        cur_param = cur_stream.parameters[0]
+        self.assertEqual(cur_param.start_time, UTCDateTime('2009-01-01T00:00:00.000000Z'))
+        self.assertEqual(cur_param.end_time, UTCDateTime('2009-02-01T00:00:00.000000Z'))
+        self.assertEqual(cur_param.gain, 32.)
+        self.assertEqual(cur_param.bitweight, 1.5895e-6)
+
+        cur_timebox = cur_stream.components[0]
+        self.assertIsInstance(cur_timebox, TimeBox)
+        self.assertEqual(cur_timebox.start_time, UTCDateTime('2009-01-01T00:00:00.000000Z'))
+        self.assertEqual(cur_timebox.end_time, UTCDateTime('2009-02-01T00:00:00.000000Z'))
+        cur_component = cur_timebox.item
+        self.assertEqual(cur_component.serial, '1417')
+        self.assertEqual(cur_component.name, 'Z')
+
+        # Test the second stream.
+        cur_stream = cur_recorder.streams[1]
+        self.assertEqual(cur_stream.name, '102')
+        self.assertEqual(cur_stream.label, 'Stream-102')
+        self.assertEqual(len(cur_stream.parameters), 1)
+        self.assertEqual(len(cur_stream.components), 1)
+
+        cur_param = cur_stream.parameters[0]
+        self.assertEqual(cur_param.start_time, UTCDateTime('2009-01-01T00:00:00.000000Z'))
+        self.assertIsNone(cur_param.end_time)
+        self.assertEqual(cur_param.gain, 32.)
+        self.assertEqual(cur_param.bitweight, 1.5895e-6)
+
+        cur_timebox = cur_stream.components[0]
+        self.assertIsInstance(cur_timebox, TimeBox)
+        self.assertEqual(cur_timebox.start_time, UTCDateTime('2009-01-01T00:00:00.000000Z'))
+        self.assertIsNone(cur_timebox.end_time)
+        cur_component = cur_timebox.item
+        self.assertEqual(cur_component.serial, '1417')
+        self.assertEqual(cur_component.name, 'N')
+
+        # Test the third stream.
+        cur_stream = cur_recorder.streams[2]
+        self.assertEqual(cur_stream.name, '103')
+        self.assertEqual(cur_stream.label, 'Stream-103')
+        self.assertEqual(len(cur_stream.parameters), 1)
+        self.assertEqual(len(cur_stream.components), 1)
+
+        cur_param = cur_stream.parameters[0]
+        self.assertEqual(cur_param.start_time, UTCDateTime('2009-01-01T00:00:00.000000Z'))
+        self.assertIsNone(cur_param.end_time)
+        self.assertEqual(cur_param.gain, 32.)
+        self.assertEqual(cur_param.bitweight, 1.5895e-6)
+
+        cur_timebox = cur_stream.components[0]
+        self.assertIsInstance(cur_timebox, TimeBox)
+        self.assertEqual(cur_timebox.start_time, UTCDateTime('2009-01-01T00:00:00.000000Z'))
+        self.assertIsNone(cur_timebox.end_time)
+        cur_component = cur_timebox.item
+        self.assertEqual(cur_component.serial, '1417')
+        self.assertEqual(cur_component.name, 'E')
+
+
+        # Test the network.
+        self.assertEqual(len(db_inventory.networks), 1)
+        cur_network = db_inventory.networks[0]
+        self.assertEqual(cur_network.name, 'XX')
+        self.assertEqual(cur_network.type, 'network type')
+        self.assertEqual(cur_network.description, 'Network description.')
+        self.assertEqual(len(cur_network.stations), 1)
+
+        # Test the station.
+        cur_station = cur_network.stations[0]
+        self.assertEqual(cur_station.name, 'GILA')
+        self.assertEqual(cur_station.location, '00')
+        self.assertEqual(cur_station.x, 15.887788)
+        self.assertEqual(cur_station.y, 47.69577)
+        self.assertEqual(cur_station.z, 643.0)
+        self.assertEqual(cur_station.coord_system, 'epsg:4326')
+        self.assertEqual(cur_station.description, 'Grillenberg')
+        self.assertEqual(len(cur_station.channels), 3)
+
+        # Test the first channel.
+        cur_channel = cur_station.channels[0]
+        self.assertEqual(cur_channel.name, 'HHZ')
+        self.assertEqual(cur_channel.description, 'Description for channel HHZ.')
+        self.assertEqual(len(cur_channel.streams), 1)
+        cur_timebox = cur_channel.streams[0]
+        self.assertIsInstance(cur_timebox, TimeBox)
+        self.assertEqual(cur_timebox.start_time, UTCDateTime('2010-02-04T00:00:00.000000Z'))
+        self.assertIsNone(cur_timebox.end_time)
+        cur_stream = cur_timebox.item
+        self.assertEqual(cur_stream.serial, '9D6C')
+        self.assertEqual(cur_stream.name, '101')
+        self.assertEqual(cur_stream.label, 'Stream-101')
+
+        # Test the second channel.
+        cur_channel = cur_station.channels[1]
+        self.assertEqual(cur_channel.name, 'HHN')
+        self.assertEqual(cur_channel.description, 'Description for channel HHN.')
+        self.assertEqual(len(cur_channel.streams), 1)
+        cur_timebox = cur_channel.streams[0]
+        self.assertIsInstance(cur_timebox, TimeBox)
+        self.assertEqual(cur_timebox.start_time, UTCDateTime('2010-02-04T00:00:00.000000Z'))
+        self.assertIsNone(cur_timebox.end_time)
+        cur_stream = cur_timebox.item
+        self.assertEqual(cur_stream.serial, '9D6C')
+        self.assertEqual(cur_stream.name, '102')
+        self.assertEqual(cur_stream.label, 'Stream-102')
+
+
+        # Test the third channel.
+        cur_channel = cur_station.channels[2]
+        self.assertEqual(cur_channel.name, 'HHE')
+        self.assertEqual(cur_channel.description, 'Description for channel HHE.')
+        self.assertEqual(len(cur_channel.streams), 1)
+        cur_timebox = cur_channel.streams[0]
+        self.assertIsInstance(cur_timebox, TimeBox)
+        self.assertEqual(cur_timebox.start_time, UTCDateTime('2010-02-04T00:00:00.000000Z'))
+        self.assertIsNone(cur_timebox.end_time)
+        cur_stream = cur_timebox.item
+        self.assertEqual(cur_stream.serial, '9D6C')
+        self.assertEqual(cur_stream.name, '103')
+        self.assertEqual(cur_stream.label, 'Stream-103')
 
 
 
