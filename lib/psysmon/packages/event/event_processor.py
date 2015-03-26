d6a0bbba5577c61168ce5f4004c0fddf1ea3fc19
Stefan Mertl <info@stefanmertl.com>
2015-03-26 15:51:14 +0100

    Added the concept for the event processor execution.

--- lib/psysmon/packages/event/event_processor.py
+++ lib/psysmon/packages/event/event_processor.py
@@ -31,19 +31,22 @@ This module contains the classes of the importWaveform dialog window.
 '''
 
 import copy
+import logging
+import psysmon
 from psysmon.core.packageNodes import CollectionNode
 from obspy.core.utcdatetime import UTCDateTime
 import psysmon.core.preferences_manager as psy_pm
 from psysmon.core.gui_preference_dialog import ListbookPrefDialog
 from plugins_event_selector import EventListField
 from psysmon.packages.tracedisplay.plugins_processingstack import PStackEditField
+from psysmon.core.processingStack import ProcessingStack
 
 
 
 ## Documentation for class importWaveform
 # 
 # 
-class EventProcessor(CollectionNode):
+class EventProcessorNode(CollectionNode):
 
     name = 'event processor'
     mode = 'editable'
@@ -86,7 +89,31 @@ class EventProcessor(CollectionNode):
 
 
     def execute(self, prevNodeOutput={}):
-        pass
+        processing_stack = ProcessingStack(name = 'pstack',
+                                           project = self.project,
+                                           nodes = self.pref_manager.get_value('processing_stack'))
+
+        # Get the output directory from the pref_manager. If no directory is
+        # specified create one based on the node resource id.
+        output_dir = ''
+
+        processor = EventProcessor(project = self.project,
+                                  processing_stack = processing_stack,
+                                  output_dir = output_dir)
+
+        if self.pref_manager.get_value('select_individual') is True:
+            events = self.pref_manager.get_value('events')
+            event_ids = [x.id for x in events]
+        else:
+            event_ids = None
+
+        processor.process(start_time = self.pref_manager.get_value('start_time'),
+                          end_time = self.pref_manager.get_value('end_time'),
+                          stations = self.pref_manager.get_value('stations'),
+                          channels = self.pref_manager.get_value('channels'),
+                          event_ids = event_ids)
+
+
 
 
     def create_selector_preferences(self):
@@ -226,3 +253,71 @@ class EventProcessor(CollectionNode):
             self.pref_manager.get_item('events')[0].disable_gui_element()
             self.pref_manager.get_item('load_events')[0].disable_gui_element()
 
+
+
+
+class EventProcessor(object):
+
+    def __init__(self, project, output_dir, processing_stack = None):
+        ''' Initialize the instance.
+
+        '''
+        # The logging logger instance.
+        logger_prefix = psysmon.logConfig['package_prefix']
+        loggerName = logger_prefix + "." + __name__ + "." + self.__class__.__name__
+        self.logger = logging.getLogger(loggerName)
+
+        self.project = project
+
+        self.processing_stack = processing_stack
+
+        self.ouput_dir = output_dir
+
+
+    def process(self, start_time, end_time, stations, channels, event_ids = None):
+        ''' Start the detection.
+
+        Parameters
+        ----------
+        start_time : :class:`~obspy.core.utcdatetime.UTCDateTime`
+            The start time of the timespan for which to detect the events.
+
+        end_time : :class:`~obspy.core.utcdatetime.UTCDateTime`
+            The end time of the timespan for which to detect the events.
+
+        stations : list of Strings
+            The names of the stations to process.
+
+        channels : list of Strings
+            The names of the channels to process.
+
+        interval : float
+            The interval into which the time span is split to run successive detections.
+
+        '''
+        events = []
+        if event_ids is None:
+            # Load the events for the given time span from the database.
+            pass
+        else:
+            # Load the events with the given ids from the database. Ignore the
+            # time-span.
+            pass
+
+
+
+        for cur_event in events:
+            # Load the waveform data for the event and the given stations and
+            # channels.
+            stream = self.load_event_waveform(scnl)
+
+            # Execute the processing stack.
+            self.processing_stack.execute(stream)
+
+            # Put the results of the processing stack into the results bag.
+            results = self.processing_stack.get_results()
+            self.result_bag.add(results)
+
+
+        # Save the processing results to files.
+        self.result_bag.save(output_dir = self.output_dir)
