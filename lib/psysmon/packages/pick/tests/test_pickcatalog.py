8b95c52d55631f7fd2bed3bdd92f32c43c3dcc2e
Stefan Mertl <stefan@mertl-research.at>
2015-04-23 19:18:34 +0200

    Fixed the clearing of the database tables. Added some test cases to test the interaction with the database.

--- lib/psysmon/packages/pick/tests/test_pickcatalog.py
+++ lib/psysmon/packages/pick/tests/test_pickcatalog.py
@@ -39,9 +39,8 @@ from obspy.core.utcdatetime import UTCDateTime
 import psysmon
 
 from psysmon.core.test_util import create_psybase
-from psysmon.core.test_util import create_empty_project
+from psysmon.core.test_util import create_full_project
 from psysmon.core.test_util import drop_project_database_tables
-from psysmon.core.test_util import clear_project_database_tables
 from psysmon.core.test_util import remove_project_filestructure
 
 import psysmon.packages.pick.core as pick_core
@@ -54,7 +53,7 @@ class PickCatalogTestCase(unittest.TestCase):
     def setUpClass(cls):
         # Configure the logger.
         logger = logging.getLogger('psysmon')
-        logger.setLevel('DEBUG')
+        logger.setLevel('INFO')
         logger.addHandler(psysmon.getLoggerHandler())
 
         # Set the data path.
@@ -63,7 +62,7 @@ class PickCatalogTestCase(unittest.TestCase):
 
         # Create an empty project.
         cls.psybase = create_psybase()
-        cls.project = create_empty_project(cls.psybase)
+        cls.project = create_full_project(cls.psybase)
         cls.project.dbEngine.echo = True
 
     @classmethod
@@ -82,7 +81,13 @@ class PickCatalogTestCase(unittest.TestCase):
 
     def tearDown(self):
         self.psybase.project_server.unregister_data()
-        clear_project_database_tables(self.project)
+
+        # Clear the picks database tables.
+        tables_to_clear = ['pick', 'pick_catalog']
+        for cur_name in tables_to_clear:
+            cur_table = self.project.dbTables[cur_name]
+            self.project.dbEngine.execute(cur_table.__table__.delete())
+
 
     def test_catalog_creation(self):
         ''' Test the pSysmon Event class.
@@ -100,40 +105,38 @@ class PickCatalogTestCase(unittest.TestCase):
         self.assertListEqual(catalog.picks, [])
 
 
-    def test_add_events(self):
-        ''' Test the add_events method.
+    def test_add_picks(self):
+        ''' Test the add_picks method.
         '''
-        catalog = ev_core.Catalog(name = 'test')
+        catalog = pick_core.Catalog(name = 'test')
 
-        # Create an event.
-        start_time = '2000-01-01T00:00:00'
-        end_time = '2000-01-01T01:00:00'
-        creation_time = UTCDateTime()
-        event = ev_core.Event(start_time = start_time,
-                      end_time = end_time,
-                      creation_time = creation_time)
+        # Create the pick
+        pick_time = UTCDateTime('2011-01-01T00:00:00')
+        channel = self.project.geometry_inventory.get_channel(name = 'HHZ', station = 'SITA')
+        channel = channel[0]
+        pick = pick_core.Pick(label = 'P',
+                              time = pick_time,
+                              amp1 = 10,
+                              channel = channel)
+        catalog.add_picks([pick,])
 
-        catalog.add_events([event,])
-
-        self.assertEqual(len(catalog.events), 1)
-        self.assertEqual(catalog.events[0], event)
-        self.assertEqual(event.parent, catalog)
+        self.assertEqual(len(catalog.picks), 1)
+        self.assertEqual(catalog.picks[0], pick)
+        self.assertEqual(pick.parent, catalog)
 
 
     def test_write_to_database(self):
         ''' Test the write_to_database method.
         '''
-        creation_time = UTCDateTime()
-        catalog = ev_core.Catalog(name = 'test',
-                          description = 'A test description.',
-                          agency_uri = 'uot',
-                          author_uri = 'tester',
-                          creation_time = creation_time)
+        catalog = pick_core.Catalog(name = 'test',
+                                    description = 'A test description.',
+                                    agency_uri = 'uot',
+                                    author_uri = 'tester')
         catalog.write_to_database(self.project)
 
-        db_catalog_orm = self.project.dbTables['event_catalog']
+        catalog_orm_class = self.project.dbTables['pick_catalog']
         db_session = self.project.getDbSession()
-        result = db_session.query(db_catalog_orm).all()
+        result = db_session.query(catalog_orm_class).all()
         db_session.close()
         self.assertEqual(len(result), 1)
         tmp = result[0]
@@ -141,105 +144,69 @@ class PickCatalogTestCase(unittest.TestCase):
         self.assertEqual(tmp.description, 'A test description.')
         self.assertEqual(tmp.agency_uri, 'uot')
         self.assertEqual(tmp.author_uri, 'tester')
-        self.assertEqual(tmp.creation_time, creation_time.isoformat())
 
 
-    def test_write_to_database_with_events(self):
+    def test_write_to_database_with_picks(self):
         ''' Test the writing to the database of a catalog with events.
         '''
-        creation_time = UTCDateTime()
-        catalog = ev_core.Catalog(name = 'test',
-                          description = 'A test description.',
-                          agency_uri = 'uot',
-                          author_uri = 'tester',
-                          creation_time = creation_time)
+        catalog = pick_core.Catalog(name = 'test',
+                                    description = 'A test description.',
+                                    agency_uri = 'uot',
+                                    author_uri = 'tester')
 
-        # Create an event.
-        start_time = '2000-01-01T00:00:00'
-        end_time = '2000-01-01T01:00:00'
-        creation_time = UTCDateTime()
-        event = ev_core.Event(start_time = start_time,
-                      end_time = end_time,
-                      creation_time = creation_time)
-
-        catalog.add_events([event,])
+        # Create a pick.
+        pick_time = UTCDateTime('2011-01-01T00:00:00')
+        channel = self.project.geometry_inventory.get_channel(name = 'HHZ', station = 'SITA')
+        channel = channel[0]
+        pick1 = pick_core.Pick(label = 'P',
+                               time = pick_time,
+                               amp1 = 10,
+                               channel = channel)
+        pick2 = pick_core.Pick(label = 'S',
+                               time = pick_time + 10,
+                               amp1 = 20,
+                               channel = channel)
+        catalog.add_picks([pick1, pick2])
 
         catalog.write_to_database(self.project)
 
-        db_catalog_orm = self.project.dbTables['event_catalog']
+
+        catalog_orm_class = self.project.dbTables['pick_catalog']
         db_session = self.project.getDbSession()
-        result = db_session.query(db_catalog_orm).all()
-        db_session.close()
+        result = db_session.query(catalog_orm_class).all()
         self.assertEqual(len(result), 1)
         tmp = result[0]
-        self.assertEqual(len(tmp.events), 1)
-        self.assertEqual(tmp.events[0].ev_catalog_id, catalog.db_id)
+        self.assertEqual(len(tmp.picks), 2)
+        self.assertEqual(tmp.picks[0].catalog_id, catalog.db_id)
+        db_session.close()
 
-        # Add a second event.
-        start_time = '2000-01-02T00:00:00'
-        end_time = '2000-01-02T01:00:00'
-        creation_time = UTCDateTime()
-        event = ev_core.Event(start_time = start_time,
-                      end_time = end_time,
-                      creation_time = creation_time)
+        # Add a third pick and update the catalog in the database.
+        channel = self.project.geometry_inventory.get_channel(name = 'HHZ', station = 'GUWA')
+        channel = channel[0]
+        pick3 = pick_core.Pick(label = 'P',
+                               time = pick_time,
+                               amp1 = 30,
+                               channel = channel)
 
-        catalog.add_events([event,])
+        catalog.add_picks([pick3,])
         catalog.write_to_database(self.project)
 
         db_session = self.project.getDbSession()
-        result = db_session.query(db_catalog_orm).all()
-        db_session.close()
+        result = db_session.query(catalog_orm_class).all()
+
         self.assertEqual(len(result), 1)
         tmp = result[0]
-        self.assertEqual(len(tmp.events), 2)
-        self.assertEqual(tmp.events[0].ev_catalog_id, catalog.db_id)
-        self.assertEqual(tmp.events[1].ev_catalog_id, catalog.db_id)
-
-
-
-    def test_write_bulletin_to_database(self):
-        ''' Test the import of a bulletin into the database.
-        '''
-        bulletin_file = os.path.join(self.data_path, 'bulletin_ims1.0_1.txt')
-        parser = ev_bulletin.ImsParser()
-        parser.parse(bulletin_file)
-        catalog = parser.get_catalog(name = 'REB', agency_uri = 'REB')
-
-        catalog.write_to_database(self.project)
-
-        db_catalog_orm = self.project.dbTables['event_catalog']
-        db_session = self.project.getDbSession()
-        result = db_session.query(db_catalog_orm).all()
+        self.assertEqual(len(tmp.picks), 3)
+        self.assertEqual(tmp.picks[0].catalog_id, catalog.db_id)
+        self.assertEqual(tmp.picks[1].catalog_id, catalog.db_id)
+        self.assertEqual(tmp.picks[2].catalog_id, catalog.db_id)
         db_session.close()
-        self.assertEqual(len(result), 1)
-        tmp = result[0]
-        self.assertEqual(tmp.name, 'REB')
-        self.assertEqual(len(tmp.events), 1)
-        cur_event = tmp.events[0]
-        self.assertEqual(cur_event.public_id, '112460')
-        self.assertEqual(cur_event.description, 'Southeast of Honshu, Japan')
-
-        # Clear the database tables.
-        clear_project_database_tables(self.project)
-
-        bulletin_file = os.path.join(self.data_path, 'bulletin_zamg_ims1.0_1.txt')
-        parser = ev_bulletin.ImsParser()
-        parser.parse(bulletin_file)
-        catalog = parser.get_catalog(name = 'ZAMG_AUTODRM', agency_uri = 'ZAMG')
-
-        catalog.write_to_database(self.project)
-
-        db_catalog_orm = self.project.dbTables['event_catalog']
-        db_session = self.project.getDbSession()
-        result = db_session.query(db_catalog_orm).all()
-        db_session.close()
-        self.assertEqual(len(result), 1)
 
 
 
 
 def suite():
-    return unittest.makeSuite(EventCatalogTestCase, 'test')
+    return unittest.makeSuite(PickCatalogTestCase, 'test')
 
 
 if __name__ == '__main__':
