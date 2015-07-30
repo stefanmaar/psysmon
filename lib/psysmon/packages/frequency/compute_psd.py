8e6bf02d62381b3cb724d45a3a9264287cccf490
Stefan Mertl <stefan@mertl-research.at>
2015-07-30 14:17:45 +0200

    Finished the modul to compute the PSDs.

--- lib/psysmon/packages/frequency/compute_psd.py
+++ lib/psysmon/packages/frequency/compute_psd.py
@@ -28,11 +28,15 @@ The importWaveform module.
     http://www.gnu.org/licenses/gpl-3.0.html
 
 '''
+import os
+import shelve
 
 import psysmon.core.packageNodes
 import psysmon.core.preferences_manager as psy_pm
 from psysmon.core.gui_preference_dialog import ListbookPrefDialog
+import obspy.core
 from obspy.core.utcdatetime import UTCDateTime
+import matplotlib.mlab as mlab
 
 
 class ComputePsdNode(psysmon.core.packageNodes.CollectionNode):
@@ -172,6 +176,8 @@ class ComputePsdNode(psysmon.core.packageNodes.CollectionNode):
         window_overlap = self.pref_manager.get_value('window_overlap')
         start_time = self.pref_manager.get_value('start_time')
         end_time = self.pref_manager.get_value('end_time')
+        psd_nfft = self.pref_manager.get_value('psd_nfft')
+        psd_overlap = self.pref_manager.get_value('psd_overlap')
 
 	# Split the timespan into processing chunks.
         overlap_length = window_length * (1 - window_overlap/100.)
@@ -180,31 +186,127 @@ class ComputePsdNode(psysmon.core.packageNodes.CollectionNode):
         window_list = [start_time + x * overlap_length for x in range(windows_between)]
 	window_list[0] = start_time
 
+        save_day = UTCDateTime(year = start_time.year,
+                               month = start_time.month,
+                               day = start_time.day)
 
         # Process each SCNL
         for cur_scnl in self.pref_manager.get_value('scnl_list'):
             # Get the channel instance from the inventory.
+            cur_channel = self.project.geometry_inventory.get_channel(station = cur_scnl[0],
+                                                                      network = cur_scnl[2],
+                                                                      location = cur_scnl[3],
+                                                                      name = cur_scnl[1])
+            if len(cur_channel) == 0:
+                self.logger.error("No channel found for scnl: %s", cur_scnl)
+                continue
+            elif len(cur_channel) > 1:
+                self.logger.error("Multiple channels found for scnl: %s; channels: %s", cur_scnl, cur_channel)
+            else:
+                cur_channel = cur_channel[0]
 
-            window_psd = []
+            psd_data = {}
 
             # Compute the psd for each window.
-            for k, cur_window in enumerate(window_list):
-                print "Computing window %s." % str(cur_window)
+            for k, cur_window_start in enumerate(window_list):
+                print "Computing window %s." % str(cur_window_start)
 
                 # If the current window starts on a new day, save the old
                 # window_psd to a daily file.
+                if cur_window_start - save_day > 86400:
+                    self.save_psd_data(psd_data, cur_scnl)
+                    save_day = UTCDateTime(year = cur_window_start.year,
+                                           month = cur_window_start.month,
+                                           day = cur_window_start.day)
+                    psd_data = {}
+
+                # Get the active stream(s) from the channel.
+                geom_stream = cur_channel.get_stream(start_time = cur_window_start,
+                                                     end_time = cur_window_start + window_length)
 
                 # Get the waveform data.
+                cur_stream = self.request_stream(start_time = cur_window_start,
+                                                 end_time = cur_window_start + window_length,
+                                                 scnl = [cur_scnl,])
+                cur_stream = cur_stream.copy()
 
-                # Detrend the data.
+                if cur_stream:
+                    self.logger.info("Processing stream %s.", cur_stream)
+                    # Detrend the data.
+                    try:
+                        cur_stream = cur_stream.split()
+                        cur_stream.detrend(type = 'constant')
+                    except Exception as e:
+                        self.logger.error('Error when processing the stream %s:\n%s', str(cur_stream), e)
+                        continue
 
-                # Get the sensor parameters.
+                    # Merge the stream again.
+                    cur_stream = cur_stream.merge()
 
-                # If sensor parameters are available: convert to velocity.
+                    # Get the sensor parameters.
 
-                # Compute the PSD using matplotlib.mlab.psd
+                    # If sensor parameters are available: convert to velocity.
 
-                # Append the psd data to the window_psd list.
+                    # Compute the PSD using matplotlib.mlab.psd
+                    n_overlap = psd_nfft / 100 * psd_overlap
+                    (P, frequ) = mlab.psd(cur_stream.traces[0].data,
+                                          Fs = cur_stream.traces[0].stats.sampling_rate,
+                                          NFFT = psd_nfft,
+                                          noverlap = n_overlap)
+
+                    if P.ndim == 2:
+                        P = P.squeeze()
+
+                    # Append the psd data to the window_psd list.
+                    #psd_data.append((cur_window_start, frequ, P, window_length, window_overlap,
+                    #                   psd_nfft, psd_overlap, cur_scnl))
+                    cur_psd = {}
+                    cur_psd['frequ'] = frequ
+                    cur_psd['P'] = P
+                    cur_psd['window_length'] = window_length
+                    cur_psd['window_overlap'] = window_overlap
+                    cur_psd['psd_nfft'] = psd_nfft
+                    cur_psd['psd_overlap'] = psd_overlap
+                    cur_psd['scnl'] = cur_scnl
+                    psd_data[cur_window_start.isoformat()] = cur_psd
+                else:
+                    #psd_data.append((cur_window_start, None, None, window_length, window_overlap,
+                    #                   psd_nfft, psd_overlap, cur_scnl))
+                    cur_psd = {}
+                    cur_psd['frequ'] = None
+                    cur_psd['P'] = None
+                    cur_psd['window_length'] = window_length
+                    cur_psd['window_overlap'] = window_overlap
+                    cur_psd['psd_nfft'] = psd_nfft
+                    cur_psd['psd_overlap'] = psd_overlap
+                    cur_psd['scnl'] = cur_scnl
+                    psd_data[cur_window_start.isoformat()] = cur_psd
+
+            # Save the remaining psd data.
+            if psd_data:
+                self.save_psd_data(psd_data, cur_scnl)
+
+
+
+    def save_psd_data(self, psd_data, scnl):
+        ''' Save the psd data to a file.
+        '''
+        if not psd_data:
+            return
+
+        output_dir = self.pref_manager.get_value('output_dir')
+        dir_name = os.path.join(output_dir, scnl[0])
+        if not os.path.exists(dir_name):
+            os.mkdir(dir_name)
+
+        first_time = UTCDateTime(sorted(psd_data.keys())[0])
+
+        filename = '%s_%s.psd' % (first_time.strftime('%Y%m%d'), '_'.join(scnl))
+        filename = os.path.join(dir_name, filename)
+        db = shelve.open(filename)
+        db['psd_data'] = psd_data
+        db.close()
+        self.logger.info("Saved PSD data in file %s.", filename)
 
 
 
