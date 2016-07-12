41fea3832cf94956a3d3d335973487f10612d8f3
Stefan Mertl <stefan@mertl-research.at>
2016-07-12 14:41:33 +0200

    Created the sourcemap module.

---
+++ lib/psysmon/packages/sourcemap/__init__.py
@@ -0,0 +1,30 @@
+# LICENSE
+#
+# This file is part of pSysmon.
+#
+# If you use pSysmon in any program or publication, please inform and
+# acknowledge its author Stefan Mertl (stefan@mertl-research.at).
+#
+# pSysmon is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+name = "sourcemap"
+version = "0.0.1"
+author = "Stefan Mertl"
+minPsysmonVersion = "0.0.1"
+description = "The sourcemap package."
+website = "http://www.mertl-research.at"
+
+# Specify the module(s) where to search for collection node classes.
+collection_node_modules = []
+
