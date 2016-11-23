928ef21593315b6c7403f992c26dec6eb4acd6b2
Stefan Mertl <stefan@mertl-research.at>
2016-11-23 17:11:37 +0100

    Finished a first working version of the particle motion tool.

--- lib/psysmon/packages/polarization_analysis/plugin_particle_motion.py
+++ lib/psysmon/packages/polarization_analysis/plugin_particle_motion.py
@@ -21,12 +21,15 @@
 import logging
 import wx
 
-
+import numpy as np
+import obspy.core.utcdatetime
 import matplotlib as mpl
 try:
     from matplotlib.backends.backend_wxagg import FigureCanvas
 except:
     from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
+
+
 
 import psysmon.core.plugins
 import psysmon.artwork.icons as icons
@@ -54,10 +57,12 @@ class ParticleMotion(psysmon.core.plugins.InteractivePlugin):
         self.icons['active'] = icons.iconsBlack16.emotion_smile_icon_16
         self.cursor = wx.CURSOR_CROSS
 
-        self.figure = None
-        self.axes = {}
+        self.frame = None
         self.start_time = None
         self.end_time = None
+
+        # TODO: Make this a preference.
+        self.channel_map = {'z': 'HHZ', 'y':'HHN', 'x':'HHE'}
 
 
     def activate(self):
@@ -86,7 +91,7 @@ class ParticleMotion(psysmon.core.plugins.InteractivePlugin):
         hooks = {}
 
         hooks['button_press_event'] = self.on_button_press
-        #hooks['button_release_event'] = self.on_button_release
+        hooks['button_release_event'] = self.on_button_release
 
         return hooks
 
@@ -110,22 +115,68 @@ class ParticleMotion(psysmon.core.plugins.InteractivePlugin):
             return
         else:
             # Check for the required 3-component traces.
-            pass
+
+            # Initialize the time window.
+            self.start_time = obspy.core.utcdatetime.UTCDateTime(event.xdata)
+            self.end_time = obspy.core.utcdatetime.UTCDateTime(event.xdata)
+
+            # Clear the particle motion plot.
+            self.frame.clear_axes()
 
             # Call the plot_particle_motion method.
+            self.plot_start_points(event, parent)
 
             # Register the motion_notify_event.
-            #hook = {}
-            #hook['motion_notify_event'] = self.plot_particle_motion
-            #cur_view.set_mpl_event_callbacks(hook, parent = parent)
+            hook = {}
+            hook['motion_notify_event'] = self.plot_particle_motion
+            cur_view.set_mpl_event_callbacks(hook, parent = parent)
 
 
-    def plot_particle_motion(self, event, parent):
-        ''' Update the particle motion plot.
+    def on_button_release(self, event, parent):
+        ''' Handle the mouse button release event.
+        '''
+        # Clear the motion notify callbacks.
+        self.view.clear_mpl_event_callbacks('motion_notify_event')
+
+
+    def plot_start_points(self, event, parent = None):
+        ''' Plot the start point markers.
         '''
         pass
 
 
+    def plot_particle_motion(self, event, parent = None):
+        ''' Update the particle motion plot.
+        '''
+        self.end_time = obspy.core.utcdatetime.UTCDateTime(event.xdata)
+        props = self.view.props
+        proc_stream = self.parent.visible_data
+        x_stream = proc_stream.select(station = props.station,
+                                      channel = self.channel_map['x'],
+                                      network = props.network,
+                                      location = props.location)
+        y_stream = proc_stream.select(station = props.station,
+                                      channel = self.channel_map['y'],
+                                      network = props.network,
+                                      location = props.location)
+        z_stream = proc_stream.select(station = props.station,
+                                      channel = self.channel_map['z'],
+                                      network = props.network,
+                                      location = props.location)
+
+        x_stream = x_stream.slice(starttime = self.start_time, endtime = self.end_time)
+        y_stream = y_stream.slice(starttime = self.start_time, endtime = self.end_time)
+        z_stream = z_stream.slice(starttime = self.start_time, endtime = self.end_time)
+
+        self.frame.plot_xy(x_stream.traces[0].data, y_stream.traces[0].data)
+        self.frame.plot_xz(x_stream.traces[0].data, z_stream.traces[0].data)
+        self.frame.plot_yz(y_stream.traces[0].data, z_stream.traces[0].data)
+
+        self.frame.set_axes_limits()
+
+        self.frame.canvas.draw()
+        self.frame.Refresh()
+        self.frame.Update()
 
 
 class ParticleMotionFrame(wx.Frame):
@@ -133,7 +184,7 @@ class ParticleMotionFrame(wx.Frame):
     '''
 
     def __init__(self, parent = None, id = wx.ID_ANY,
-                 title = 'particle motion', size = (600, 600), dpi = 300):
+                 title = 'particle motion', size = (600, 600), dpi = 90):
         ''' Initialize the instance.
         '''
         wx.Frame.__init__(self,
@@ -148,6 +199,12 @@ class ParticleMotionFrame(wx.Frame):
         self.figure = mpl.figure.Figure(None, dpi=dpi, facecolor='white')
         self.canvas = FigureCanvas(self, -1, self.figure)
 
+        self.axes = {}
+        self.axes['xy'] = self.figure.add_subplot(2, 2, 1)
+        self.axes['xz'] = self.figure.add_subplot(2, 2, 3)
+        self.axes['yz'] = self.figure.add_subplot(2, 2, 4)
+        self.annotate_axes()
+
         self.canvas.SetMinSize((30, 10))
         self.SetBackgroundColour('white')
 
@@ -155,3 +212,85 @@ class ParticleMotionFrame(wx.Frame):
         self.sizer = wx.BoxSizer(wx.VERTICAL)
         self.sizer.Add(self.canvas, 1, wx.EXPAND)
         self.SetSizer(self.sizer)
+
+        # The particle motion lines.
+        self.lines = {}
+        self.lines['xy'] = None
+        self.lines['xz'] = None
+        self.lines['yz'] = None
+
+
+    def annotate_axes(self):
+        ''' Annotate the axes.
+        '''
+        self.axes['xy'].set_xlabel('x')
+        self.axes['xy'].set_ylabel('y')
+
+        self.axes['xz'].set_xlabel('x')
+        self.axes['xz'].set_ylabel('z')
+
+        self.axes['yz'].set_xlabel('y')
+        self.axes['yz'].set_ylabel('z')
+
+        for cur_axes in self.axes.itervalues():
+            cur_axes.set_aspect('equal', adjustable = 'box')
+
+
+    def clear_axes(self):
+        ''' Clear all plots in the axes.
+        '''
+        for cur_key, cur_axes in self.axes.iteritems():
+            if self.lines[cur_key] is not None:
+                cur_axes.lines.remove(self.lines[cur_key])
+                del self.lines[cur_key]
+                self.lines[cur_key] = None
+
+
+    def plot_xy(self, x_data, y_data):
+        ''' Plot the xy particle motion.
+        '''
+        self.plot(x = x_data, y = y_data, mode = 'xy')
+
+
+    def plot_xz(self, x_data, z_data):
+        ''' Plot the xy particle motion.
+        '''
+        self.plot(x = x_data, y = z_data, mode = 'xz')
+
+
+    def plot_yz(self, y_data, z_data):
+        ''' Plot the xy particle motion.
+        '''
+        self.plot(x = y_data, y = z_data, mode = 'yz')
+
+
+    def plot(self, x, y, mode):
+        ''' Plot data in the corresponding axes.
+
+        '''
+        cur_axes = self.axes[mode]
+        cur_line = self.lines[mode]
+        if cur_line is None:
+            cur_lines = cur_axes.plot(x, y, 'k')
+            self.lines[mode] = cur_lines[0]
+        else:
+            cur_line.set_xdata(x)
+            cur_line.set_ydata(y)
+
+
+    def set_axes_limits(self):
+        ''' Adjust the axes limits.
+        '''
+        max_amp = []
+        for cur_line in self.lines.itervalues():
+            cur_x_max = np.max(np.abs(cur_line.get_xdata()))
+            cur_y_max = np.max(np.abs(cur_line.get_ydata()))
+            max_amp.append(np.max([cur_x_max, cur_y_max]))
+        max_amp = np.max(max_amp)
+
+        for cur_axes in self.axes.itervalues():
+            cur_axes.set_xlim((-max_amp, max_amp))
+            cur_axes.set_ylim((-max_amp, max_amp))
+
+
+
