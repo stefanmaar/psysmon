27ae7c01da949fd8dfad3f68b949aca7830e3161
Stefan Mertl <stefan@mertl-research.at>
2017-05-01 16:25:56 +0200

    Improved the sorting of nested nodes. Still not working for mixed stations and arrays.

--- lib/psysmon/core/gui_view.py
+++ lib/psysmon/core/gui_view.py
@@ -185,6 +185,8 @@ class Viewport(wx.lib.scrolledpanel.ScrolledPanel):
 
             self.node_list = sorted_nodes
             self.rearrange_nodes()
+            sorted_nodes = []
+
 
 
 
@@ -450,6 +452,18 @@ class ContainerNode(wx.Panel):
         for cur_node in self.node_list:
             cur_node.blit()
 
+
+    def sort_nodes(self, keys = None, order = None):
+        ''' Sort the containers nodes.
+        '''
+        if order:
+            sorted_nodes = []
+            for cur_order in order:
+                cur_node = self.get_node(recursive = False, **cur_order)
+                sorted_nodes.extend(cur_node)
+
+            self.node_list = sorted_nodes
+            self.rearrange_nodes()
 
 
 class ViewContainerNode(wx.Panel):
