730c1d3d25899a02071bbd53c1cfa1b36101f225
Stefan Mertl <stefan@mertl-research.at>
2018-05-15 16:21:51 +0200

    Added the pgv and dom_frequ of stations DUBA and DUBAM.

--- lib/psysmon/packages/mss_network/publish_report.py
+++ lib/psysmon/packages/mss_network/publish_report.py
@@ -121,21 +121,6 @@ class MssPublishBlastReport(package_nodes.CollectionNode):
         stream : :class:`obspy.core.Stream`
             The data to process.
         '''
-        # Download the quarry information file.
-        src_filename = self.pref_manager.get_value('filename')
-        tmp_fid, tmp_filename = tempfile.mkstemp(prefix = 'quarry_validation',
-                                        dir = self.project.tmpDir)
-        ftp = ftplib.FTP(host = self.pref_manager.get_value('host'),
-                         user = self.pref_manager.get_value('username'),
-                         passwd = self.pref_manager.get_value('password'))
-        try:
-            with open(tmp_filename, 'wb') as fp:
-                ftp.retrbinary('RETR ' + src_filename, fp.write)
-        finally:
-            ftp.quit()
-            os.close(tmp_fid)
-
-
         # Load the quarry_blast information.
         blast_filename = self.pref_manager.get_value('blast_file')
         if os.path.exists(blast_filename):
@@ -146,53 +131,117 @@ class MssPublishBlastReport(package_nodes.CollectionNode):
             raise RuntimeError("Couldn't open the blast file %s.", blast_filename)
 
 
-        # Parse the quarry information file.
         export_rows = []
-        with open(tmp_filename, 'r') as fp:
-            reader = csv.DictReader(fp, delimiter = ';')
-            for cur_row in reader:
-                if cur_row['Sprengnummer'] in quarry_blast.keys():
-                    cur_blast = quarry_blast[cur_row['Sprengnummer']]
-                    if 'psysmon_event_id' in cur_blast.keys():
-                        cur_export_row = {}
-                        cur_export_row['Sprengnummer'] = cur_row['Sprengnummer']
-                        cur_export_row['ID'] = cur_blast['id']
-                        cur_export_row['time [UTC]'] = cur_blast['event_time'].isoformat()
-                        cur_export_row['network_mag'] = round(cur_blast['magnitude']['network_mag'], 2)
-                        cur_export_row['network_mag_std'] = round(cur_blast['magnitude']['network_mag_std'], 2)
-                        max_pgv = max(cur_blast['max_pgv']['data'].values())
-                        max_pgv_ind = cur_blast['max_pgv']['data'].values().index(max_pgv)
-                        cur_export_row['max_pgv [mm/s]'] = round(max_pgv * 1000, 3)
-                        cur_export_row['max_pgv_station'] = cur_blast['max_pgv']['data'].keys()[max_pgv_ind]
-                        export_rows.append(cur_export_row)
-                    else:
-                        self.logger.info("No related result found for blast %s.", cur_row['Sprengnummer'])
 
+
+        # Parse the quarry information file.
+        use_export_sprengung = False
+        if use_export_sprengung:
+            # Download the quarry information file.
+            src_filename = self.pref_manager.get_value('filename')
+            tmp_fid, tmp_filename = tempfile.mkstemp(prefix = 'quarry_validation',
+                                            dir = self.project.tmpDir)
+            ftp = ftplib.FTP(host = self.pref_manager.get_value('host'),
+                             user = self.pref_manager.get_value('username'),
+                             passwd = self.pref_manager.get_value('password'))
+            try:
+                with open(tmp_filename, 'wb') as fp:
+                    ftp.retrbinary('RETR ' + src_filename, fp.write)
+            finally:
+                ftp.quit()
+                os.close(tmp_fid)
+
+            with open(tmp_filename, 'r') as fp:
+                reader = csv.DictReader(fp, delimiter = ';')
+                for cur_row in reader:
+                    if cur_row['Sprengnummer'] in quarry_blast.keys():
+                        cur_blast = quarry_blast[cur_row['Sprengnummer']]
+                        if 'psysmon_event_id' in cur_blast.keys() and 'computed_on' in cur_blast.keys():
+                            cur_export_row = {}
+                            cur_export_row['Sprengnummer'] = cur_row['Sprengnummer']
+                            cur_export_row['ID'] = cur_blast['id']
+                            cur_export_row['time [UTC]'] = cur_blast['event_time'].isoformat()
+                            cur_export_row['network_mag'] = round(cur_blast['magnitude']['network_mag'], 2)
+                            cur_export_row['network_mag_std'] = round(cur_blast['magnitude']['network_mag_std'], 2)
+                            max_pgv = max(cur_blast['max_pgv']['data'].values())
+                            max_pgv_ind = cur_blast['max_pgv']['data'].values().index(max_pgv)
+                            cur_export_row['max_pgv [mm/s]'] = round(max_pgv * 1000, 3)
+                            cur_export_row['max_pgv_station'] = cur_blast['max_pgv']['data'].keys()[max_pgv_ind]
+                            if 'DUBA:MSSNet:00' in cur_blast['dom_stat_frequ'].keys():
+                                cur_export_row['dom_frequ_duba [Hz]'] = round(cur_blast['dom_stat_frequ']['DUBA:MSSNet:00'], 2)
+                            else:
+                                cur_export_row['dom_frequ_duba [Hz]'] = ''
+
+                            if 'DUBAM:MSSNet:00' in cur_blast['dom_stat_frequ'].keys():
+                                cur_export_row['dom_frequ_dubam [Hz]'] = round(cur_blast['dom_stat_frequ']['DUBAM:MSSNet:00'], 2)
+                            else:
+                                cur_export_row['dom_frequ_dubam [Hz]'] = ''
+
+                            export_rows.append(cur_export_row)
+                        else:
+                            self.logger.info("No related result found for blast %s.", cur_row['Sprengnummer'])
+        else:
+            for cur_blast_key in sorted(quarry_blast.keys()):
+                cur_blast = quarry_blast[cur_blast_key]
+                if 'psysmon_event_id' in cur_blast.keys() and 'computed_on' in cur_blast.keys():
+                    cur_export_row = {}
+                    cur_export_row['Sprengnummer'] = cur_blast_key
+                    cur_export_row['ID'] = cur_blast['id']
+                    cur_export_row['time [UTC]'] = cur_blast['event_time'].isoformat()
+                    cur_export_row['network_mag'] = round(cur_blast['magnitude']['network_mag'], 2)
+                    cur_export_row['network_mag_std'] = round(cur_blast['magnitude']['network_mag_std'], 2)
+                    max_pgv = max(cur_blast['max_pgv']['data'].values())
+                    max_pgv_ind = cur_blast['max_pgv']['data'].values().index(max_pgv)
+                    cur_export_row['max_pgv [mm/s]'] = round(max_pgv * 1000, 3)
+                    cur_export_row['max_pgv_station'] = cur_blast['max_pgv']['data'].keys()[max_pgv_ind]
+
+                    if 'DUBA:MSSNet:00' in cur_blast['max_pgv']['data'].keys():
+                        cur_export_row['pgv_duba [mm/s]'] = round(cur_blast['max_pgv']['data']['DUBA:MSSNet:00'] * 1000, 3)
+                    else:
+                        cur_export_row['pgv_duba [mm/s]'] = ''
+
+                    if 'DUBA:MSSNet:00' in cur_blast['dom_stat_frequ'].keys():
+                        cur_export_row['dom_frequ_duba [Hz]'] = round(cur_blast['dom_stat_frequ']['DUBA:MSSNet:00'], 2)
+                    else:
+                        cur_export_row['dom_frequ_duba [Hz]'] = ''
+
+                    if 'DUBAM:MSSNet:00' in cur_blast['max_pgv']['data'].keys():
+                        cur_export_row['pgv_dubam [mm/s]'] = round(cur_blast['max_pgv']['data']['DUBAM:MSSNet:00'] * 1000, 3)
+                    else:
+                        cur_export_row['pgv_dubam [mm/s]'] = ''
+
+                    if 'DUBAM:MSSNet:00' in cur_blast['dom_stat_frequ'].keys():
+                        cur_export_row['dom_frequ_dubam [Hz]'] = round(cur_blast['dom_stat_frequ']['DUBAM:MSSNet:00'], 2)
+                    else:
+                        cur_export_row['dom_frequ_dubam [Hz]'] = ''
+
+                    export_rows.append(cur_export_row)
+                else:
+                    self.logger.info("No related result found for blast %s.", cur_blast_key)
 
         if export_rows:
             # Upload the overall result file.
             export_filepath = os.path.join(self.project.tmpDir, 'sprengungen_auswertung.csv')
             with open(export_filepath, 'w') as fp:
                 fieldnames = ['ID', 'Sprengnummer', 'time [UTC]', 'network_mag',
-                              'network_mag_std', 'max_pgv [mm/s]', 'max_pgv_station']
+                              'network_mag_std', 'max_pgv [mm/s]', 'max_pgv_station',
+                              'pgv_duba [mm/s]', 'dom_frequ_duba [Hz]',
+                              'pgv_dubam [mm/s]', 'dom_frequ_dubam [Hz]']
                 writer = csv.DictWriter(fp, fieldnames = fieldnames)
                 writer.writeheader()
                 writer.writerows(export_rows)
 
             # Upload the result files.
-            ftp = ftplib.FTP(host = self.pref_manager.get_value('host'),
-                             user = self.pref_manager.get_value('username'),
-                             passwd = self.pref_manager.get_value('password'))
-            try:
-                with open(export_filepath, 'r') as fp:
-                    ftp.storbinary('STOR ' + os.path.basename(export_filepath), fp)
-            except:
-                self.logger.exception("Problems when uploading the result file.")
-            finally:
-                ftp.quit()
-                os.close(export_filepath)
-
-
-
-
+            upload = True
+            if upload:
+                ftp = ftplib.FTP(host = self.pref_manager.get_value('host'),
+                                 user = self.pref_manager.get_value('username'),
+                                 passwd = self.pref_manager.get_value('password'))
+                try:
+                    with open(export_filepath, 'r') as fp:
+                        ftp.storbinary('STOR ' + os.path.basename(export_filepath), fp)
+                except:
+                    self.logger.exception("Problems when uploading the result file.")
+                finally:
+                    ftp.quit()
 
