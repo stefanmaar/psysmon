046fc586925f14eb955980bb163712c526aa5e49
Stefan Mertl <info@stefanmertl.com>
2018-11-20 16:53:36 +0100

    Added some amplitude features.

--- lib/psysmon/packages/core_looper_children/compute_amplitude_features.py
+++ lib/psysmon/packages/core_looper_children/compute_amplitude_features.py
@@ -41,6 +41,11 @@ class ComputeAmplitudeFeatures(package_nodes.LooperCollectionChildNode):
         '''
         package_nodes.LooperCollectionChildNode.__init__(self, **args)
 
+        # The gap between the noise end and the event begin for SNR
+        # computation [s].
+        self.noise_gap = 0.1
+
+
         pref_page = self.pref_manager.add_page('Preferences')
         w_group = pref_page.add_group('window')
 
@@ -52,6 +57,14 @@ class ComputeAmplitudeFeatures(package_nodes.LooperCollectionChildNode):
                               tool_tip = 'The length of the time-span used to compute the noise parameters [s].'
                              )
         w_group.add_item(item)
+
+
+    @property
+    def pre_stream_length(self):
+        ''' The time-span needed for correct processing prior to the start time
+        of the stream passed to the execute method [s].
+        '''
+        return self.pref_manager.get_value('noise_window_length') + self.noise_gap
 
 
     def edit(self):
@@ -77,14 +90,20 @@ class ComputeAmplitudeFeatures(package_nodes.LooperCollectionChildNode):
             The data to process.
         '''
         # Create a table result.
-        columns = ['max_abs', 'peak_to_peak']
-        table_result = result.TableResult(name = 'amplitude features',
-                                          key_name = 'scnl',
-                                          start_time = process_limits[0],
-                                          end_time = process_limits[1],
-                                          origin_name = self.name,
-                                          origin_resource = origin_resource,
-                                          column_names = columns)
+        columns = ['max_abs', 'peak_to_peak', 'mean', 'std', 'median', 'snr', 'snr_max_mean', 'snr_max_max']
+        if 'event' in kwargs:
+            event_id = kwargs['event'].db_id
+            #columns = ['event_id'].extend(columns)
+        else:
+            result_postfix = None
+        table_result = result.TableResult(name='amplitude features',
+                                          key_name='scnl',
+                                          start_time=process_limits[0],
+                                          end_time=process_limits[1],
+                                          origin_name=self.name,
+                                          origin_resource=origin_resource,
+                                          column_names=columns,
+                                          event_id = event_id)
 
         for tr in stream.traces:
             if process_limits is not None:
@@ -102,11 +121,46 @@ class ComputeAmplitudeFeatures(package_nodes.LooperCollectionChildNode):
             # Compute the maximum range of the trace.
             peak_to_peak = np.max(proc_trace.data) + np.abs(np.min(proc_trace.data))
 
+            # Compute the mean value of the trace.
+            mean = np.mean(proc_trace.data)
+
+            # Compute the standard deviation of the trace.
+            std = np.std(proc_trace.data)
+
+            # Compute the median of the trace.
+            median = np.median(proc_trace.data)
+
+            #Compute the signal to noise ratio.
+            if process_limits is None:
+                snr = np.NaN
+                snr_max_mean = np.NaN
+                snr_max_max = np.NaN
+            else:
+                noise_window_length = self.pref_manager.get_value('noise_window_length')
+                noise_start_time = process_limits[0] - noise_window_length - self.noise_gap
+                if noise_start_time < tr.stats.starttime:
+                    noise_start_time = tr.stats.starttime
+                noise_trace = tr.slice(starttime = noise_start_time,
+                                      endtime = noise_start_time + noise_window_length)
+                signal_rms = np.sqrt(np.mean(proc_trace.data**2))
+                noise_rms = np.sqrt(np.mean(noise_trace.data**2))
+                noise_mean = np.mean(np.abs(noise_trace.data))
+                noise_max_abs = np.max(np.abs(noise_trace.data))
+                snr = signal_rms/noise_rms
+                snr_max_mean = max_abs / noise_mean
+                snr_max_max = max_abs / noise_max_abs
+
 
             cur_scnl = p_util.traceid_to_scnl(tr.id)
             table_result.add_row(key = cur_scnl,
                                  max_abs = max_abs,
-                                 peak_to_peak = peak_to_peak)
+                                 peak_to_peak = peak_to_peak,
+                                 mean = mean,
+                                 std = std,
+                                 median = median,
+                                 snr = round(snr,2),
+                                 snr_max_mean = round(snr_max_mean, 2),
+                                 snr_max_max = round(snr_max_max, 2))
 
 
         self.result_bag.add(table_result)
