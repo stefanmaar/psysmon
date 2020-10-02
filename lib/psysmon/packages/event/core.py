faedc289f713752f1e0f7a6988cef1d12a094e4d
Stefan Mertl <info@stefanmertl.com>
2020-10-02 18:13:19 +0200

    Handle the event_id correctly when updating an event in the database.

--- lib/psysmon/packages/event/core.py
+++ lib/psysmon/packages/event/core.py
@@ -173,7 +173,12 @@ class Event(object):
                                     pref_origin_id = None,
                                     pref_magnitude_id = None,
                                     pref_focmec_id = None,
-                                    ev_type_id = None,
+                                    # TODO: Add an event_type class to handle
+                                    # the event types correctly. Currently, the
+                                    # event_type attribute holds the event type
+                                    # id.
+                                    tags = ','.join(self.tags),
+                                    ev_type_id = self.event_type,
                                     ev_type_certainty = self.event_type_certainty,
                                     description = self.description,
                                     agency_uri = self.agency_uri,
@@ -218,7 +223,7 @@ class Event(object):
                 #db_event.pref_origin_id = self.pref_origin_id
                 #db_event.pref_magnitude_id = self.pref_magnitude_id
                 #db_event.pref_focmec_id = self.pref_focmec_id
-                db_event.ev_type = self.event_type
+                db_event.ev_type_id = self.event_type
                 db_event.ev_type_certainty = self.event_type_certainty
                 db_event.tags = ','.join(self.tags)
                 db_event.agency_uri = self.agency_uri
