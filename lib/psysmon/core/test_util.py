f8fdfb9443a03ca9708009355e12f0eac83df08a
Stefan Mertl <info@stefanmertl.com>
2022-03-07 15:49:59 +0100

    Cleaned file to make it work with headless mode.

--- lib/psysmon/core/test_util.py
+++ lib/psysmon/core/test_util.py
@@ -34,7 +34,6 @@ import numpy as np
 
 import psysmon
 import psysmon.core.base
-import psysmon.core.gui as psygui
 from psysmon.core.project import User
 from psysmon.packages.geometry.inventory_parser import InventoryXmlParser
 from psysmon.packages.geometry.db_inventory import DbInventory
@@ -319,50 +318,6 @@ def clean_unittest_database():
     drop_database_tables(db_dialect, db_driver, db_user, db_pwd, db_host, db_name, project_name)
 
 
-
-
-
-def prepare_project(test_case):
-        # Configure the logger.
-        logger = logging.getLogger('psysmon')
-        logger.setLevel(psysmon.logConfig['level'])
-        logger.addHandler(psysmon.getLoggerHandler())
-
-        # Get the pSysmon base directory.
-        psyBaseDir = '/home/stefan/01_gtd/04_aktuelleProjekte/pSysmon/01_src/psysmon/lib/psysmon/'
-        psyBaseDir = os.path.dirname(psyBaseDir)
-
-        # Initialize the pSysmon base object.
-        psyBase = Base(psyBaseDir)
-        #psyBase.scan4Package()
-
-        # Load the pSysmon test project.
-        path = "/home/stefan/01_gtd/04_aktuelleProjekte/pSysmon/03_pSysmonProjects/test/test.ppr"
-        psyBase.loadPsysmonProject(path)
-
-        # Quest for the user and the database password.
-        psyBase.project.setActiveUser('stefan','')
-
-        # Load the database structure of the project packages.
-        psyBase.project.loadDatabaseStructure(psyBase.packageMgr.packages)
-
-        # Create the project waveclient.
-        waveclient = PsysmonDbWaveClient('main client', psyBase.project)
-        psyBase.project.addWaveClient(waveclient)
-        waveclient = EarthwormWaveClient('earthworm localhost')
-        psyBase.project.addWaveClient(waveclient)
-        test_case.app =psygui.PSysmonApp()
-
-        nodeTemplate = psyBase.packageMgr.getCollectionNodeTemplate('tracedisplay')
-        test_case.node = copy.deepcopy(nodeTemplate)
-        test_case.node.project = psyBase.project
-
-        # Create the node logger. This is usually done in the collection.
-        loggerName = __name__ + "." + test_case.node.__class__.__name__
-        test_case.node.logger = logging.getLogger(loggerName)
-
-
-
 def compute_synthetic_seismogram(length, sps, wavelet_offset, amp = 1., snr = None):
     ''' Compute a synthetic seismogram.
     '''
@@ -380,7 +335,6 @@ def compute_synthetic_seismogram(length, sps, wavelet_offset, amp = 1., snr = No
     seismo = seismo * amp
 
     return seismo
-
 
 
 def compute_wavelet(amp, sps):
